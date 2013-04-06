1909563934b12ef4e5dc851a6c9d59f859a4b1b0
Alex Thaler <alex.thaler@orbitz.com>
2013-04-05 19:02:40 -0500

    added build grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,26 +2,46 @@ module.exports = function(grunt) {
 
   // A very basic default task.
   grunt.registerTask('default', 'Log some stuff.', function() {
-    grunt.log.write('Logging some stuff...').ok();
+      grunt.log.write('Logging some stuff...').ok();
   });
 
 
-  grunt.registerTask("preview", "Calls through to the wintersmith preview function", function (prop) {
-    var done = this.async();
+  grunt.registerTask("preview", "Calls through to wintersmith preview", function (prop) {
+      var done = this.async();
+      grunt.log.write("Calling wintersmith preview: check localhost:8080");
 
-    grunt.log.write("Calling wintersmith preview: ");
+      grunt.util.spawn({
+          cmd : "wintersmith",
+          args : [ "preview" ]
+      }, function (err, result) {
+          if (err) {
+            grunt.log.error("holy crap!" + err);
+            return done(false);
+          }
 
-    grunt.util.spawn({
-      cmd : "wintersmith",
-      args : [ "preview" ]
-    }, function (err, result) {
-      if (err) {
-        grunt.log.error("holy crap!" + err);
-        return done(false);
-      }
+          grunt.log.writeln(result).ok();
+          done(true);
+      });
+  });
 
-      grunt.log.writeln(result).ok();
-      done(true);
-    });
+  grunt.registerTask("build", "Calls through to wintersmith build, builds out the generated site", function (prop) {
+      var done = this.async();
+      grunt.log.write("Calling wintersmith build");
+
+      grunt.util.spawn({
+        cmd : "wintersmith",
+        args : ["build"]
+      }, function(err, result) {
+        if(err) {
+          grunt.log.error("holy crap!" + err).fail();
+          return done(false);
+        }
+          grunt.log.writeln("generating site").ok();
+          done(true);
+      });
+  });
+
+  grunt.registerTask("publish", "publish", function() {
+      var done = this.async();
   });
 };
